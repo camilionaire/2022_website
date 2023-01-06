857b994bfbb82f319e926828f66befbe388911e0
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2023-01-06 01:02:47 -0800

    min 60 in tutorial... hero does not show?

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar></Navbar>
+        <Navbar />
         <Routes>
-          <Route path="/" exact component={Home} />
+          <Route path="/" component={Home} />
         </Routes>
       </Router>
     </div>
